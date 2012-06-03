e8f9518e3255c67ff1f72c39eb9eba176826298b
Shoichi Mori <suzuki_tokyo1003@yahoo.co.jp>
2012-06-03 17:30:01 +0900

    Signed-off-by: Shoichi Mori <suzuki_tokyo1003@yahoo.co.jp>

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+
+	$(".scroll").click(function(event){
+
+		event.preventDefault();
+        
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+
+		$('html, body').animate({scrollTop:target_top}, 500);
+	});
+});
+/** end of Function **/
