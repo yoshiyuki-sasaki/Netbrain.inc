2ce7ab6fece00bf893ca4cb56a84875d98d53644
Shoichi Mori <suzuki_tokyo1003@yahoo.co.jp>
2012-06-19 12:42:38 +0900

    delete img folder

--- js/common.js
+++ js/common.js
@@ -1,3 +1,6 @@
+/* =======================
+ click and smooth slidert
+ ========================*/
 $(function() {
 
 	$(".scroll").click(function(event){
@@ -15,3 +18,46 @@ $(function() {
 	
 });
 /** end of Function **/
+
+/* =======================
+ pre-loader activate
+ ========================*/
+function ImageLoader(){
+
+	var timer;
+	$('body').jpreLoader({
+		splashID: "#jSplash",
+		splashFunction: function() {  //passing Splash Screen script to jPreLoader
+			$('#jSplash').children('section').not('.selected').hide();
+			$('#jSplash').hide().fadeIn(800);
+			
+			timer = setInterval(function() {
+				splashRotator();
+			}, 5000);
+		}
+	}, function() {	//jPreLoader callback function
+		clearInterval(timer);
+	});
+}
+
+/* =======================
+ pre-loader movie control
+ ========================*/
+function splashRotator(){
+
+	var cur = $('#jSplash').children('.selected');
+	var next = $(cur).next();
+
+	if($(next).length != 0) {
+		$(next).addClass('selected');
+	} else {
+		$('#jSplash').children('section:first-child').addClass('selected');
+		next = $('#jSplash').children('section:first-child');
+	}
+	
+	$(cur).removeClass('selected').fadeOut(800, function() {
+		$(next).fadeIn(800);
+	});
+}
+
+
